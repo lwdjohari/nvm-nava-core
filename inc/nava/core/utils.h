83b02a21161c5b1a1e9200cc79a059a72afb0ada
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-22 12:44:24 +0800

    Initial CMake cxx17 projects.

---
+++ inc/nava/core/utils.h
@@ -0,0 +1,18 @@
+#ifndef NAVA_CORE_V2_UTILS_H
+#define NAVA_CORE_V2_UTILS_H
+
+namespace nvm {
+namespace nava {
+namespace core {
+
+class Utils {
+ private:
+ public:
+  Utils();
+  ~Utils();
+};
+
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
+#endif
---
+++ src/nava/core/utils.cc
@@ -0,0 +1,13 @@
+#include "nava/core/utils.h"
+
+namespace nvm {
+namespace nava {
+namespace core {
+
+Utils::Utils() {}
+
+Utils::~Utils() {}
+
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
